0e8940e4c7d10e0ad0c04906fea28d7a95026f86
jiro.kirameki <ec2-user@ip-172-31-34-226.us-east-2.compute.internal>
2018-06-26 08:20:55 +0000

    heart

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,7 +6,7 @@
             <div class="cover-contents">
                 <h2>思い出に残るデートプランを見つけよう</h2>
                 @if (!Auth::check())
-                <a href="#" class="btn btn-success btn-lg">DATESPOTSを始める</a>
+                <a href="#" class="btn btn-success btn-lg">❤ DATESPOTSを始める ❤</a>
                 @endif
             </div>
         </div>
